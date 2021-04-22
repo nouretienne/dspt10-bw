6749e7c7e857f5a94e3d7ac8591c89d4eec28632
mariokart345 <sladek.ben@gmail.com>
2021-04-22 08:59:43 -0400

    added decorators for homepage functionality

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
 import uvicorn
 
-from app import db, ml, viz
+from app import db, ml, viz, aws
 
 description = """
 Edit your app's title and description. See [https://fastapi.tiangolo.com/tutorial/metadata/](https://fastapi.tiangolo.com/tutorial/metadata/)
@@ -16,12 +20,26 @@ To use these interactive docs:
 """
 
 app = FastAPI(
-    title='DS API',
-    description=description,
-    docs_url='/',
+    title='Spotify Suggester ref',
+    description="Predict songs that you'll like based on what you already like",
+    docs_url='/docs',
 )
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory='templates')
+
+
+@app.get('/', tags=['Homepage'])
+async def homepage(request: Request):
+    return templates.TemplateResponse('homepage.html', context={'request': request})
+
+
+@app.post('/', tags=['Homepage'])
+async def homepage(request: Request, song_name: str = Form(...), genre: str = Form(...), release_date: int = Form(...), explicit: int = Form(...)):
+    return templates.TemplateResponse('homepage.html', context={'request': request, 'song_name': song_name, 'genre': genre, 'release_date': release_date, 'explicit': explicit})
+
 app.include_router(db.router, tags=['Database'])
+app.include_router(aws.router, tags=['AWS S3'])
 app.include_router(ml.router, tags=['Machine Learning'])
 app.include_router(viz.router, tags=['Visualization'])
 
