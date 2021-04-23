3dbef3e0250387008cb830d78673f8ba5662a33c
Doesnt <Matter>
2021-04-23 09:57:41 -0400

    added remote decorator to interact with functions utilizing spotipy from browser

--- app/spotify.py
+++ app/spotify.py
@@ -2,12 +2,14 @@ import json
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from fastapi import APIRouter, HTTPException
 import dotenv
 
 dotenv.load_dotenv(dotenv.find_dotenv())
 SPOTIPY_CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
 SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
 
+router = APIRouter()
 auth_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
@@ -20,3 +22,9 @@ def song_to_artist(name):
         if results["tracks"]["items"][i]["artists"][0]["name"] not in art:
             art.append(results["tracks"]["items"][i]["artists"][0]["name"])
     return art
+
+
+@router.get('/spotify/get_artists')
+async def spotify(song_name):
+    artists = song_to_artist(song_name)
+    return {'artists': artists}
