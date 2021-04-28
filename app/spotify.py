9b3752a7bbe2614034127661febeaf42113410f7
mariokart345 <sladek.ben@gmail.com>
2021-04-28 03:30:21 -0400

    updated functions and added function for making predictions

--- app/spotify.py
+++ app/spotify.py
@@ -3,6 +3,9 @@ import os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from fastapi import APIRouter
+from joblib import load
+import pandas as pd
+import numpy as np
 import dotenv
 
 dotenv.load_dotenv(dotenv.find_dotenv())
@@ -12,6 +15,23 @@ SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
 router = APIRouter()
 auth_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET)
 sp = spotipy.Spotify(auth_manager=auth_manager)
+
+features = ["duration_ms", "danceability", "energy", "key", "loudness", "mode", "speechiness",
+            "acousticness", "instrumentalness", "liveness", "valence", "tempo", "time_signature"]
+
+full_features = ['popularity', 'duration_ms', 'explicit', 'danceability',
+                 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness',
+                 'instrumentalness', 'liveness', 'valence', 'tempo', 'time_signature',
+                 'release_date']
+
+engineered_full_features = ['popularity', 'duration_ms', 'explicit', 'danceability',
+                            'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness',
+                            'instrumentalness', 'liveness', 'valence', 'tempo', 'time_signature',
+                            'year_release']
+
+original_tracks = pd.read_csv('app/tracks.csv')
+mMs = load('app/MinMaxScaler_model.joblib')
+NN = load('app/NN_model.joblib')
 
 
 def song_to_artist(name):
@@ -24,16 +44,43 @@ def song_to_artist(name):
     return art
 
 
-def same_name_songs(name):
-    results = sp.search(q='track:' + name, type='track', limit=20)
+def track_features(track_id, popularity, explicit, year_release):
+    tid = "spotify:track:" + track_id
+    analysis = sp.audio_features(tid)
+    out = [analysis[0][x] for x in features]
+    out.insert(0, popularity)
+    out.insert(2, explicit)
+    out.append(year_release)
+    return pd.Series(list(out))
+
+
+def same_name_songs(song, artist):
+    results = sp.search(q='artist:' + artist + ' track:' + song, type='track', limit=1)
     table = []
     for i in range(len(results['tracks']['items'])):
-        if results['tracks']['items'][i]['artists'][0]['name'] not in table[0]['name']:
-            table.append({'name': results['tracks']['items'][i]['artists'][0]['name'],
-                          'picture': results['tracks']['items'][i]['album']['images'][1]['url'],
-                          'link': results['tracks']['items'][i]['external_urls']['spotify'],
-                          'sample': results['tracks']['items'][i]['preview_url']})
+        track_feat = [track_features(results['tracks']['items'][i]['id'],
+                                     results['tracks']['items'][i]['popularity'],
+                                     np.int64(results['tracks']['items'][i]['explicit']),
+                                     pd.to_datetime(pd.Series(
+                                         results['tracks']['items'][i]['album']['release_date'])).dt.year[
+                                         0]).values]
+        table.append({'link': results['tracks']['items'][i]['external_urls']['spotify'],
+                      'picture': results['tracks']['items'][i]['album']['images'][0]['url'],
+                      'name': results['tracks']['items'][i]['artists'][0]['name'],
+                      'sample': results['tracks']['items'][i]['preview_url'],
+                      'track_id': results['tracks']['items'][i]['id'],
+                      'features': mMs.transform(np.array(track_feat))})
     return table
+
+
+def suggestion(name):  # j number in the same_name_songs list
+    liste_recommendationn = NN.kneighbors(name)[1][0]
+    recom = []
+    for i in range(10):
+        recom.append({'artist': original_tracks.iloc[liste_recommendationn[i]][['artists']],
+                      'link': 'https://open.spotify.com/track/' + original_tracks.iloc[liste_recommendationn[i]][['id']].values[0],
+                      'id': original_tracks.iloc[liste_recommendationn[i]][['id']].values[0]})
+    return recom
 
 
 def song_list_to_sample(song_list, artists):
